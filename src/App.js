154370ac5c1579e27a7065a8f6622f7cb58b38bd
Ysael Saez <ysael.saez@outlook.com>
2021-12-25 15:44:48 -0400

    add Hero component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>uNotes, the beginning...</h1>
+      <Hero />
     </div>
   );
 }
